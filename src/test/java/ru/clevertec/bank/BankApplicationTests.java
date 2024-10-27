babb216919ac0648c0cc20674fcaab05c6c1bb78
pashpashovich <pavel.kosovich2004@gmail.com>
2024-10-27 19:07:37 +0300

    init commit

---
+++ src/test/java/ru/clevertec/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
